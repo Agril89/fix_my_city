e6a8164d7ff7a9722a47315913c04054dee68117
Agril89 <luca.roatta@gmail.com>
2017-04-06 13:22:02 +0200

    new add table

--- db.php
+++ db.php
@@ -15,7 +15,7 @@ $sql = "CREATE TABLE persons(
     email VARCHAR(70) NOT NULL UNIQUE
 )";
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, $sql);
+$result = $pg_conn -> exec($sql);
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
