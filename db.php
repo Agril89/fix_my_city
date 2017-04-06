bf7a71687b4c2a24ec7ebc96121cf8fee7e370a1
Agril89 <luca.roatta@gmail.com>
2017-04-06 13:00:30 +0200

    create table

--- db.php
+++ db.php
@@ -17,4 +17,29 @@ if (!pg_num_rows($result)) {
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 print "\n";
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("ec2-54-217-232-100.eu-west-1.compute.amazonaws.com", "rstohmylvowjnb", "sbspmI4zY55-iOzzUtYDoaWkFH", "deo4uema1tk2q6");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt create table query execution
+$sql = "CREATE TABLE persons(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(70) NOT NULL UNIQUE
+)";
+if(mysqli_query($link, $sql)){
+    echo "Table created successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
 ?>
