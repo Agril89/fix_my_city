784ecee41113c1b13689cadc5d3abcbddc1f4d7f
Simone <simohu95@gmail.com>
2017-04-06 10:54:31 +0200

    TestPhp1.0.2

--- register.php
+++ register.php
@@ -1,16 +1,11 @@
-<?php
-    echo "<h1>Hello World</h1>"
-    echo $_POST['Username']
-    
-?>
-
 <html>
 <body>
 
-    Welcome <?php echo $_POST["Username"]; 
+    Welcome
+    <?php
+        echo $_POST["Username"]; 
         echo "<h1>Hello World</h1>"
-    
-    ?><br>
+    ?>
 
 </body>
 </html>
