8781a84d8dd1e538d72d02f89ecd26eb16f272f4
Agril89 <luca.roatta@gmail.com>
2017-04-11 12:37:26 +0200

    added login.php

--- login.php
+++ login.php
@@ -9,10 +9,10 @@ or DIE('query non riuscita'.pg_error());
 // Con il SELECT qua sopra selezione dalla tabella users l utente registrato (se lo è) con i parametri che mi ha passato il form di login, quindi
 // Quelli dentro la variabile POST. username e password.
 if(pg_num_rows($query) != 1){        //se c'è una persona con quel nome nel db allora loggati
-$row = pg_fetch_assoc($query); // metto i risultati dentro una variabile di nome $row
-$_SESSION["logged"] =true;  // Nella variabile SESSION associo TRUE al valore logge
-header("location:map.html"); // e mando per esempio ad una pagina esempio.php// in questo caso rimanderò ad una pagina prova.php
+	$row = pg_fetch_assoc($query); // metto i risultati dentro una variabile di nome $row
+	$_SESSION["logged"] =true;  // Nella variabile SESSION associo TRUE al valore logge
+	header("location:map.html"); // e mando per esempio ad una pagina esempio.php// in questo caso rimanderò ad una pagina prova.php
 }else{
-echo "non ti sei registrato con successo"; // altrimenti esce scritta a video questa stringa di errore
+	echo "non ti sei registrato con successo"; // altrimenti esce scritta a video questa stringa di errore
 }
 ?>
